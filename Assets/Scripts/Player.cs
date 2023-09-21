26d8aeef6bc7410cd9b1449b495eff1e75b3a7b8
Patrick <logicalrocketship@gmail.com>
2023-09-20 21:28:36 -0400

    XP - Patrick
    added xp system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+
+    // accleeration is by default set to 80, maxSpeed is set to 10, and deceleration is set to 30
     Vector2 velocity = Vector2.zero;
     [SerializeField]
     float acceleration;
@@ -12,7 +14,19 @@ public class Player : MonoBehaviour
     [SerializeField]
     float deceleration;
 
+    //for xp mechanic 
+    //Serialization to be tested by user
+    [SerializeField]
+    int xpPoints = 0;
+    //initially serialized for display purposes only
+    int xpLevel = 0;
+
     // Start is called before the first frame update
+    void Start() 
+    {   
+        //Test the xp system with 10 xpPoints
+        addXP(10);
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,5 +41,15 @@ public class Player : MonoBehaviour
             velocity = Vector2.MoveTowards(velocity,Vector2.zero,deceleration*Time.deltaTime);
         }
         transform.position += (Vector3)(velocity * Time.deltaTime);
+    }
+
+    //current placeholder for xp function
+    void addXP(int points) 
+    {
+        xpPoints += points;
+
+        //placeholder for leveling up
+        xpLevel = (int)Mathf.Sqrt(xpPoints);
+        
     }
 }
