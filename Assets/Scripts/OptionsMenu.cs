3d4f1c890b6a3309b48fdd2f50a1ce995b6149ff
ElectroBlade0937 <sheldon.qtran@yahoo.com>
2023-09-27 20:46:10 -0400

    Options Menu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public void BackButton()
+    {
+        Instantiate(PauseMenu, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(this);
+    }
+}
--- Assets/Scripts/SetMusicVolume.cs
+++ Assets/Scripts/SetMusicVolume.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using static OptionsMenu;
 
 public class SetMusicVolume : MonoBehaviour
 {
