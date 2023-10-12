b8c317a7992dcad010e1a7f1a2b7f8e33268c664
KaratePikachu <KaidenKeyZ@gmail.com>
2023-10-11 21:14:21 -0400

    Enemy Manager
    Created a simple enemy manager to circumvent calling GetObjectWithTag during runtime. Have different enemies add their gameobject to the list when created and remove it when destroyed

--- Assets/Scripts/Equipment/Weapons/ProjectileWeapon.cs
+++ Assets/Scripts/Equipment/Weapons/ProjectileWeapon.cs
@@ -18,13 +18,13 @@ public class ProjectileWeapon : Weapon
 
     /** Projectile size multiplier. Used for level-ups. */
     [SerializeField] protected float projectileSize;
-    
+
     /** The amount of damage each projectile does. Exact details are left to the projectile script. */
     [SerializeField] protected float damage;
 
     /** Amount of projectiles to fire with each shot. */
     [SerializeField] protected int projectileCount;
-    
+
     /** How fast the projectiles start out when fired. */
     [SerializeField] protected float projectileSpeed;
 
@@ -39,13 +39,13 @@ public class ProjectileWeapon : Weapon
 
     /** True if the projectile should be attached to the player and move with them. Otherwise, it operates in world space and moves independently of the player. */
     [SerializeField] protected bool projectileLocalSpace;
-    
+
     /**
      * The object that gets initialized on each fire.
      * Object must have the Projectile component attached to it.
      */
     [SerializeField] protected GameObject projectilePrefab;
-    
+
     /**
      * Called whenever the weapon should fire, based on its `fireRate`.
      */
@@ -56,12 +56,12 @@ public class ProjectileWeapon : Weapon
         if (projectileLocalSpace) proj.transform.SetParent(EquipmentManager.instance.transform);
         if (spawnProjectileAtTarget) proj.transform.position = targetPosition;
         else proj.transform.position = EquipmentManager.instance.transform.position;
-        
+
         proj.Setup(targetPosition, damage, pierceCount, projectileSpeed, knockback, projectileSize);
         // TODO: handle projectile count
         // basic cases can be handled by just looping this, but if they have the same target, they'll need to be separated a bit
     }
-    
+
     /**
      * Returns the position to fire towards in world coordinates.
      * For targeting strategies that aim at an enemy, this will be that enemy's position.
@@ -70,14 +70,21 @@ public class ProjectileWeapon : Weapon
     protected Vector2 GetTarget()
     {
         var player = Player.instance;
-        var enemies = GameObject.FindGameObjectsWithTag("Enemy"); // TODO: this should probably be a list in some enemy manager class
+        var enemies = EnemyManager.enemies; // TODO: this should probably be a list in some enemy manager class
+
+        if (targetingStrategy==TargetType.RandomDirection||enemies.Count==0)
+        {
+            var randomRads = Random.Range(0, 2 * Mathf.PI);
+            return (Vector2)player.transform.position + new Vector2(Mathf.Cos(randomRads), Mathf.Sin(randomRads));
+        }
+
         switch (targetingStrategy)
         {
             case TargetType.WalkingDirection:
                 // TODO: handle stationary player better (remember most recent walking direction?)
                 return (Vector2) player.transform.position + player.velocity;
             case TargetType.RandomEnemy:
-                return enemies[Random.Range(0, enemies.Length)].transform.position;
+                return enemies[Random.Range(0, enemies.Count)].transform.position;
             case TargetType.NearestEnemy:
                 var min = new Vector2(0, 0);
                 var minDist = 1000000f;
@@ -91,9 +98,6 @@ public class ProjectileWeapon : Weapon
                     }
                 }
                 return min;
-            case TargetType.RandomDirection:
-                var randomRads = Random.Range(0, 2 * Mathf.PI);
-                return (Vector2) player.transform.position + new Vector2(Mathf.Cos(randomRads), Mathf.Sin(randomRads));
         }
 
         throw new Exception($"Invalid targeting strategy `{targetingStrategy}`!");
