23826a0801d0fd82ceab8cb55887f5f19d12b051
Patrick <logicalrocketship@gmail.com>
2023-10-25 20:42:24 -0400

    Tutorial

---
+++ Assets/Scripts/SkipTutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTutorial : MonoBehaviour
+{
+    //current script is WIP
+    public static InTutorial = true;
+    public void Skip() {
+        InTutorial = false;
+    }
+
+}
