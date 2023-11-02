f8333757a6fe1ac7da8413606ca9923358c663cb
QuinEpstein <qepstein@gmu.edu>
2023-11-01 21:35:25 -0400

    options menu

--- Assets/Scripts/SetMusicVolume.cs
+++ Assets/Scripts/SetMusicVolume.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using static OptionMenu;
 
 public class SetMusicVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider MusicVolume;
 
-    public void SetMusic(float sliderValue)
+    public void SetMusic()
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("MusicVolume", Mathf.Log10(MusicVolume.value) * 20);
     }
 
-    
+    public void Update()
+    {
+        //MusicVolume.value = mixer.GetFloat();
+    }
 
 }
--- Assets/Scripts/SetSoundVolume.cs
+++ Assets/Scripts/SetSoundVolume.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using static OptionMenu;
 
 public class SetSoundVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider SFX;
 
-    public void SetSound(float sliderValue)
+    public void SetSound()
     {
-        mixer.SetFloat("SfxVolume", Mathf.Log10(sliderValue) * 20);
+        mixer.SetFloat("SfxVolume", Mathf.Log10(SFX.value) * 20);
     }
 }
