aaff79fdad1a020916b8a9e8068d7ad90a026c8a
Jack Whritenour <jwhriten@gmu.edu>
2023-11-01 21:45:24 -0400

    started work on boss movement

--- Assets/Scripts/Enemy/BossWanderMovement.cs
+++ Assets/Scripts/Enemy/BossWanderMovement.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Wander : IBossMovement
 {
+
+    private float movingTimer = 2;
     public void Move(float speed)
     {
-        
+
+        movingTimer -= Time.deltaTime;
+        if (movingTimer <= 0)
+        {
+
+
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            //gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, moveTowardsObject.transform.position, movementSpeed * Time.deltaTime);
+            movingTimer = 2;
+        }
     }
 }
