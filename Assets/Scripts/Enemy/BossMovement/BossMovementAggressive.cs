642964d0f58e0b19ecfe5e89edfd48c1841d79f8
Tushar <stuffcopresident@gmail.com>
2023-11-08 20:31:30 -0500

    Aggressive Movement Component implemented

---
+++ Assets/Scripts/Enemy/BossMovement/BossMovementAggressive.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovementAggressive : MonoBehaviour
+{
+    public Transform moveTowards;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(moveTowards == null)
+        {
+            moveTowards = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(moveTowards != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, moveTowards.position, speed * Time.deltaTime);
+        }
+    }
+}
