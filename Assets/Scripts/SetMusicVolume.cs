0ab17962b8e78e2bde1e7ccfcea9fbd2b5f27e12
QuinEpstein <qepstein@gmu.edu>
2023-11-08 21:58:59 -0500

    Options menu

--- Assets/Scripts/SetMusicVolume.cs
+++ Assets/Scripts/SetMusicVolume.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 using static OptionMenu;
 
 public class SetMusicVolume : MonoBehaviour
 {
     public AudioMixer mixer;
     public Slider MusicVolume;
+    public GameObject MusicText;
 
     public void SetMusic()
     {
         mixer.SetFloat("MusicVolume", Mathf.Log10(MusicVolume.value) * 20);
     }
 
-    public void Update()
+    public void Start()
     {
-        //MusicVolume.value = mixer.GetFloat();
+
     }
 
+    public void Update()
+    {
+        float num;
+        bool volExists = mixer.GetFloat("MusicVolume", out num);
+        if (volExists)
+        {
+            num = Mathf.Pow(10, (num / 20));
+            MusicVolume.value = num;
+            MusicText.GetComponent<TextMeshPro>().text = ("Music Volume: " + (num * 100) + "%");
+        }
+    } 
 }
--- Assets/Scripts/SetSoundVolume.cs
+++ Assets/Scripts/SetSoundVolume.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 using static OptionMenu;
 
 public class SetSoundVolume : MonoBehaviour
 {
     public AudioMixer mixer;
     public Slider SFX;
+    public GameObject SFXText;
 
     public void SetSound()
     {
         mixer.SetFloat("SfxVolume", Mathf.Log10(SFX.value) * 20);
+    }
+
+    public void Start()
+    {
+
+    }
+    public void Update()
+    {
+        float num;
+        bool volExists = mixer.GetFloat("SfxVolumeVolume", out num);
+        if (volExists)
+        {
+            num = Mathf.Pow(10, (num / 20));
+            SFX.value = num;
+            SFXText.GetComponent<TextMeshPro>().text = ("SFX Volume: " + (num * 100) + "%");
+        }
     }
 }
