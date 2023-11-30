0a2ffddeebc4f4d7f8e90c45887365634d5d2a8a
Tushar <stuffcopresident@gmail.com>
2023-11-29 21:39:12 -0500

    Implements player death and game over scene

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -6,14 +6,25 @@ using UnityEngine.UI;
 
 public class GameOverScript : MonoBehaviour
 {
-    public DifficultyManager difficulty;
+    //public DifficultyManager difficulty;
+    public bool IsGameOver { get; private set;}
+    public GameObject gameOverScreen;
 
+    public void DoGameOver()
+    {
+        LoadObjects();
+        IsGameOver = true;
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0; // pause the game while gameovered
+        
+    }
     //Resarting the game on easy or normal as defined in the Diffuclty Manager will restart the game, and reset upgrades(Upgrade system not implemented yet)
     public void RestartButton() {
 
             //Restarts to the currentLevel
+            Time.timeScale = 1;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
+           
     }
 
     //if in hardmode the game will restart, it may be redundant since exit game effectivley does the same thing
@@ -27,11 +38,22 @@ public class GameOverScript : MonoBehaviour
     //Exits the game
     public void ExitButton()
     {
-
+        Time.timeScale = 1;
         SceneManager.LoadScene("TitleScreenScene");
        
     }
 
+    private void Reset()
+    {
+        LoadObjects();
+    }
+    private void LoadObjects()
+    {
+        if (gameOverScreen == null)
+        {
+            gameOverScreen = this.gameObject;//theoretically should be the same
+        }
+    }
 
 }
 
