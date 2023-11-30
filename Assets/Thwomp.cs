039f30d7ef6ca27005ea36db59f871789dfce1b4
Connor Harrison <ctharrison04@gmail.com>
2023-11-29 20:34:04 -0500

    Finished Thwomp Ememy

--- Assets/Thwomp.cs
+++ Assets/Thwomp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /**
@@ -8,19 +9,59 @@ using UnityEngine;
 public class Thwomp : MonoBehaviour
 {
     private GameObject player;
+    Vector3 downVec;
+    float originalY;
+    bool hasHit;
+    bool doRise;
+    bool doFall;
+    float wait;
     // Start is called before the first frame update
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        downVec = new Vector3(0, -10, 0);
+        originalY = this.transform.position.y;
+        doRise = false;
+        doFall = false;
+        hasHit = false;
+        wait = 1;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        if(player.transform.position.x >= this.transform.position.x - 1 && player.transform.position.x <= this.transform.position.x + 1)
-        {
+        if(player.transform.position.x >= this.transform.position.x - 1 && player.transform.position.x <= this.transform.position.x + 1 && player.transform.position.y < this.transform.position.y && doRise == false && doFall == false)
+        { 
             Debug.Log("REEEEEEE");
-            Vector3.MoveTowards(this.transform.position, player.transform.position, 1);
+            doFall = true;
+        }
+        if(doFall == true)
+        {
+            this.transform.position += downVec * Time.deltaTime;
+        }
+        if(player.transform.position.y >= this.transform.position.y)
+        {
+            hasHit = true;
+            doFall = false;
+        }
+        if(hasHit == true)
+        {
+            wait -= 1 * Time.deltaTime;
+            if(wait <= 0)
+            {
+                doRise = true;
+                wait = 1;
+            }
+
+        }
+        if(doRise == true)
+        {
+            this.transform.position -= downVec * Time.deltaTime;
+            if(this.transform.position.y >= originalY)
+            {
+                doRise = false;
+                hasHit = false;
+            }
         }
     }
 
