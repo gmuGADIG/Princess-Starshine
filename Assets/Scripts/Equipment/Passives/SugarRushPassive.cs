49562c5e645701111db495ada32e4cd2de7692c9
Patrick <logicalrocketship@gmail.com>
2023-11-29 21:31:47 -0500

    Keeps changes from previous commit

--- Assets/Scripts/Equipment/Passives/SugarRushPassive.cs
+++ Assets/Scripts/Equipment/Passives/SugarRushPassive.cs
@@ -6,14 +6,6 @@ public class SugarRushPassive : Passive
     //Amount of damage dealt by weapon
     public float damageIncrease = 1.5f;
 
-    //The Player prefab
-    private GameObject playerPrefab;
-
-    public SugarRushPassive()
-    {
-        this.type = EquipmentType.SugarRush;
-    }
-    
     public override (string description, Action onApply) GetLevelUps() 
     {
         return ("Greater weapon damage", applyDamage);
@@ -21,27 +13,7 @@ public class SugarRushPassive : Passive
 
     public void applyDamage() 
     {
-        if (GameObject.FindGameObjectWithTag("Player") != null) 
-        {
-            playerPrefab = GameObject.FindGameObjectWithTag("Player");
-            if (playerPrefab.GetComponentInChildren<EquipmentManager>() != null)
-            {
-                EquipmentManager playerEquipment = playerPrefab.GetComponentInChildren<EquipmentManager>();
-
-                foreach (ProjectileWeapon weapon in playerEquipment.allWeapons)
-                {
-                    weapon.increaseDamage(damageIncrease);
-                }
-            }
-            else 
-            {
-                Debug.LogError("Player is missing 'Player' tag");
-            }
-        }
-        else 
-        {
-            Debug.LogError("Player is missing 'Player' tag");
-        }
+        ProjectileWeapon.staticStatModifiers.damage += damageIncrease;
     }
 
     public override void OnEquip() {
