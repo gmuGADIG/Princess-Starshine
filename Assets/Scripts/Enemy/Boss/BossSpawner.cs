8330b9d21624efbc314589a5517f5187c739b0da
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2023-12-02 11:30:40 -0500

    Add Boss Spawner

---
+++ Assets/Scripts/Enemy/Boss/BossSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnTime = 300f;
+    [SerializeField] GameObject bossPrefab;
+
+    Transform playerTransform;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBossCoroutine());
+        playerTransform = FindObjectOfType<Player>().transform;
+    }
+
+    IEnumerator SpawnBossCoroutine()
+    {
+        yield return new WaitForSeconds(spawnTime);
+        Instantiate(bossPrefab, playerTransform.position, Quaternion.identity);
+    }
+}
