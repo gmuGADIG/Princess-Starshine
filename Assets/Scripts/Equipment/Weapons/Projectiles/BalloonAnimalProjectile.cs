783d7fd787fbb5fd498b91933463631d5f42978a
Justin1L8 <justin.w.langdon@gmail.com>
2023-12-14 02:10:33 -0500

    Polished several weapon visuals / audio
    Added visual polish to several weapons. All untextured circles / squares have been replaced with programmer art or particles, and various visual improvements have been implemented.
    Implemented most weapon sound effects.

--- Assets/Scripts/Equipment/Weapons/Projectiles/BalloonAnimalProjectile.cs
+++ Assets/Scripts/Equipment/Weapons/Projectiles/BalloonAnimalProjectile.cs
@@ -14,6 +14,9 @@ public class BalloonAnimalProjectile : Projectile
     float inflationRate;
     float slowdownRate;
 
+    float rotationalVelocity;
+    SpriteRenderer sprite;
+
     public override void Setup(ProjectileWeapon weapon, Vector2 target, float damage, int pierceCount, float speed, float knockback, float size, float dotRate)
     {
         base.Setup(weapon, target, damage, pierceCount, speed, knockback, size, dotRate);
@@ -27,6 +30,10 @@ public class BalloonAnimalProjectile : Projectile
         inflationRate = ba.InflationRate;
         slowdownRate = speed * inflationRate;
 
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.transform.eulerAngles = new Vector3(0, 0, Random.Range(0, 360));
+        rotationalVelocity = Random.Range(-60, 60);
+
         LifetimeExpired += () => {
             SoundManager.Instance.PlaySoundGlobal(explosionSoundName);
 
@@ -36,18 +43,38 @@ public class BalloonAnimalProjectile : Projectile
                 transform.position.y,
                 5
             );
-            go.transform.localScale = new Vector3(explosionSize, explosionSize);
+            go.transform.rotation = sprite.transform.rotation;
 
             Projectile projectile = go.GetComponent<Projectile>();
             projectile.maxLifeTime = 0.3f;
-            projectile.Setup(weapon, transform.position, explosionDamage, -1, 0, 0, explosionSize, 0);
+            projectile.Setup(weapon, transform.position, explosionDamage, -1, 0, 0, this.size, 0);
+            print($"Balloon animal's size is {this.size}");
         };
     }
 
     protected override void Update() {
         base.Update();
-        size += inflationRate * Time.deltaTime;
+        UpdateSize();
         speed -= slowdownRate * Time.deltaTime;
-        transform.localScale = new Vector3(size, size);
+        transform.localScale = new Vector3(size, size, 1);
+
+        sprite.transform.rotation *= Quaternion.Euler(0, 0, rotationalVelocity * Time.deltaTime);
+    }
+
+    private void UpdateSize()
+    {
+        // set the intervals in which the balloon grows (numbers based on the inflating sound file)
+        const float
+            firstInflateStart = 0.5f,
+            firstInflateEnd = 1f,
+            secondInflateStart = 1.75f,
+            secondInflateEnd = 2.25f;
+
+        bool isInflating;
+        if (timeAlive is >= firstInflateStart and < firstInflateEnd) isInflating = true;
+        else if (timeAlive is >= secondInflateStart and < secondInflateEnd) isInflating = true;
+        else isInflating = false;
+
+        if (isInflating) size += inflationRate * Time.deltaTime;
     }
 }
