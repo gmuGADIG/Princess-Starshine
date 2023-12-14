81a1f83c367c4496e7724b55baac6f42a5a66494
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-12-14 16:05:32 -0500

    Fixed bug where player could still access next level even if they lost.
    Added static event to BossHealth script

--- Assets/Scripts/LevelPreview/LevelPreviewHelper.cs
+++ Assets/Scripts/LevelPreview/LevelPreviewHelper.cs
@@ -7,8 +7,15 @@ public class LevelPreviewHelper : MonoBehaviour
     [Tooltip("This is the scene name of the next level. (i.e. the scene name of the level preview node you want to unlock).")]
     public string NextLevelSceneName;
 
-    void OnDestroy() {
-        // this should be called when the scene is changing *fingers crossed*
+    void BossDied() {
         SaveManager.SaveData.FurthestLevelSceneName = NextLevelSceneName;
+    }
+
+    void Start() {
+        BossHealth.BossDied += BossDied;
+    }
+
+    void OnDestroy() {
+        BossHealth.BossDied -= BossDied;
     }
 }
