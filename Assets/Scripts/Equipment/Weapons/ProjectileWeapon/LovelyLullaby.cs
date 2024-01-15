15649a96038d470ed0d59363d987b6e5999b0926
Michael Campbell <rainbow@portal2d.xyz>
2024-01-14 22:54:49 -0500

    fix pretty parasol in boss cutscene; fix #313
    pressing B and M skips the level
    
    Lovely Lullaby sound effect added

--- Assets/Scripts/Equipment/Weapons/ProjectileWeapon/LovelyLullaby.cs
+++ Assets/Scripts/Equipment/Weapons/ProjectileWeapon/LovelyLullaby.cs
@@ -2,4 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LovelyLullaby : ProjectileWeapon {}
+public class LovelyLullaby : ProjectileWeapon {
+    public override void ApplyLevelUp(WeaponLevelUp levelUp)
+    {
+        base.ApplyLevelUp(levelUp);
+
+        SoundManager.Instance.PlaySoundGlobal(shootSoundName);
+    }
+}
